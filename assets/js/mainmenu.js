9b063700551e280963e1a68cf6e8d591cd06a701
johnmarkventura <33885731+johnmarkventura@users.noreply.github.com>
2017-12-05 16:50:49 -1000

    middle of third video

--- assets/js/mainmenu.js
+++ assets/js/mainmenu.js
@@ -6,12 +6,19 @@ var mainMenuState = {
     
     
     preload: function() { 
+        game.load.spritesheet('startBtn', 'assets/images/strart_btn.png',i93,)
         
+        game.stage.backgroundColor = '#8185d5';
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVerically = true;
     },
     
     
     create: function() {
+        startBtn = game.add.button(game.world.centerX-95, 200, 'startBtn', this.startGame, this, 2, 1, 0);
         
+        var spaceKey = game.input.keyboard.addKey(Phaser.keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.startGame, this);
     },
     
     
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -6,12 +6,15 @@ var menuState = {
     
     
     preload: function() { 
+        game.load.spritesheet('button', 'assets/images/retry_btn.png', 193, 71);
         
+        game.pageAlignHorizontally = true
+        game.pageAlignVertically = true
     },
     
     
     create: function() {
-        
+        button = game.add.button(game.world.centerX - 95, 200,'button', this.startGame)
     },
     
     
